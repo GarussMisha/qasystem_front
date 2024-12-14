264abfe5928f47715228262a9b97dabddc45d201
Garuss MIsha <Garussm@gmail.com>
2024-12-14 23:46:50 +0500

    Создане шаблона для HeadBar и его добавление в App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <HeadBar />
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeadBar from '@/components/HeadBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HeadBar, // Регестрируем HeadBar
+  },
+};
 </script>
 
 <style>
@@ -19,8 +21,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/HeadBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="head-bar">
+        <div class="buttons">
+            <button>Кнопка 1</button>
+            <button>Кнопка 2</button>
+            <button>Кнопка 3</button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeadBar',
+};
+</script>
+
+<style>
+.head-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: #333;
+  color: white;
+}
+
+.buttons button {
+  margin-left: 10px;
+  padding: 5px 10px;
+  background-color: #555;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.buttons button:hover {
+  background-color: #777;
+}
+</style>
