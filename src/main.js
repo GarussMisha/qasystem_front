3872c9431db4d6c60f3bab7254dd43e27c763b6d
Garuss MIsha <Garussm@gmail.com>
2024-12-14 23:14:47 +0500

    Добавление Pinia в main.js и так же создание хранилица для управления проектами projectStore.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; // Импортируем маршрутизатор
+import router from './router'; // Импортируем маршрутизатор route
+import { createPinia } from 'pinia'; // Импортируем Pinia
 
 const app = createApp(App)
 
+app.use(createPinia) // Подключаем Pinia
 app.use(router) // Подключаем маршрутизатор
 app.mount('#app')
